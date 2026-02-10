3f4880889f7d064587c779ae2641d8f52cd97684
Edward Cox <YOUR_EMAIL@example.com>
2026-02-10 17:50:08 +1100

    Simplify CLI: allow direct file execution

--- cmd/bpl/main.go
+++ cmd/bpl/main.go
@@ -3,44 +3,68 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"bpl-plus/interpreter"
 	"bpl-plus/lexer"
 	"bpl-plus/parser"
 )
 
+func usage() {
+	fmt.Println("Usage:")
+	fmt.Println("  bplplus <file.bpl>")
+	fmt.Println("  bplplus run <file.bpl>")
+	os.Exit(1)
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: bpl run <file.bpl>")
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	args := os.Args[1:]
+
+	var filename string
+
+	// Allow: bplplus run file.bpl
+	if args[0] == "run" {
+		if len(args) != 2 {
+			usage()
+		}
+		filename = args[1]
+	} else {
+		// Allow: bplplus file.bpl
+		filename = args[0]
+	}
+
+	if !strings.HasSuffix(filename, ".bpl") {
+		fmt.Fprintf(os.Stderr, "Error: expected a .bpl file, got %q\n", filename)
 		os.Exit(1)
 	}
 
-	if os.Args[1] != "run" {
-		fmt.Println("Unknown command:", os.Args[1])
-		os.Exit(1)
-	}
-
-	file := os.Args[2]
-
-	srcBytes, err := os.ReadFile(file)
+	src, err := os.ReadFile(filename)
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Fprintf(os.Stderr, "Error reading %s: %v\n", filename, err)
 		os.Exit(1)
 	}
-	src := string(srcBytes)
 
-	lx := lexer.New(src)
+	lx := lexer.New(string(src))
 	p := parser.New(lx)
 
-	prog, err := p.ParseProgram()
+	stmts, err := p.ParseProgram()
 	if err != nil {
-		fmt.Println("Parse error:", err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	interp := interpreter.NewWithSource(file, src)
-	if err := interp.Run(prog); err != nil {
-		fmt.Println(err.Error())
+	interp := interpreter.NewWithSource(
+		filepath.Base(filename),
+		string(src),
+	)
+
+	if err := interp.Run(stmts); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
